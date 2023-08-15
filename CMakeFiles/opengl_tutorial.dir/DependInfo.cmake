98b8b6ae755d722067a151786fa4f4fdaf4f701e
Pranav Desai <daftylooper@pop-os.localdomain>
2023-08-16 00:05:11 +0530

    init opengl stash boilerplate

---
+++ CMakeFiles/opengl_tutorial.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/daftylooper/Desktop/C++/opengl/include/glad.c" "CMakeFiles/opengl_tutorial.dir/include/glad.c.o" "gcc" "CMakeFiles/opengl_tutorial.dir/include/glad.c.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/Camera.cpp" "CMakeFiles/opengl_tutorial.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/Camera.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/IndexBuffer.cpp" "CMakeFiles/opengl_tutorial.dir/src/IndexBuffer.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/IndexBuffer.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/Macros.cpp" "CMakeFiles/opengl_tutorial.dir/src/Macros.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/Macros.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/Renderer.cpp" "CMakeFiles/opengl_tutorial.dir/src/Renderer.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/Renderer.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/Shader.cpp" "CMakeFiles/opengl_tutorial.dir/src/Shader.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/Shader.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/Texture.cpp" "CMakeFiles/opengl_tutorial.dir/src/Texture.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/Texture.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/VertexArray.cpp" "CMakeFiles/opengl_tutorial.dir/src/VertexArray.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/VertexArray.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/VertexBuffer.cpp" "CMakeFiles/opengl_tutorial.dir/src/VertexBuffer.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/VertexBuffer.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/main.cpp" "CMakeFiles/opengl_tutorial.dir/src/main.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/main.cpp.o.d"
+  "/home/daftylooper/Desktop/C++/opengl/src/vendor/stb_image/stb_image.cpp" "CMakeFiles/opengl_tutorial.dir/src/vendor/stb_image/stb_image.cpp.o" "gcc" "CMakeFiles/opengl_tutorial.dir/src/vendor/stb_image/stb_image.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/opengl_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_tutorial.dir/include/glad.c.o"
+  "CMakeFiles/opengl_tutorial.dir/include/glad.c.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/Camera.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/IndexBuffer.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/IndexBuffer.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/Macros.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/Macros.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/Renderer.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/Shader.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/Texture.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/VertexArray.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/VertexArray.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/VertexBuffer.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/VertexBuffer.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/main.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/main.cpp.o.d"
+  "CMakeFiles/opengl_tutorial.dir/src/vendor/stb_image/stb_image.cpp.o"
+  "CMakeFiles/opengl_tutorial.dir/src/vendor/stb_image/stb_image.cpp.o.d"
+  "opengl_tutorial"
+  "opengl_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opengl_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
